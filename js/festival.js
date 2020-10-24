549f4705df8f89f0660952204f56a9f8abdb29d4
whablog <3@qq.com>
2020-10-24 13:26:55 +0800

    Update festival.js

--- js/festival.js
+++ js/festival.js
@@ -1,3 +1,4 @@
+function al(){
 var D=new Date();
 var dl=["10-24"];
 var tx=[['1024程序员节快乐','向程序员们致敬，拒绝加班']]
@@ -15,3 +16,37 @@ if(a!=-1){
     ]
   });
 }
+}
+checkCookie()
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+            
+    } else {
+         setCookie("username", '1', 1);
+         al();
+    }
+  }
